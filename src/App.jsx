c4bd62a297e6230fb5b713667701feb9383c709d
Juan José Villarreal <jjvillarreal@Air-de-Juan.home>
2023-07-27 20:17:09 +0200

    Code refactored

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Qr } from "./components/Qr";
-import image from "./assets/image-qr-code.png";
-import HeaderLink from "./components/HeaderLinks";
-import routes from "./routes/routes.d";
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Qr } from './components/Qr';
+import image from './assets/image-qr-code.png';
+import HeaderLink from './components/HeaderLinks';
+import routes from './routes/routes.d';
 
-const titleText = "Improve your front-end skills by building projects";
+const titleText = 'Improve your front-end skills by building projects';
 const bodyText =
-  "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level";
+  'Scan the QR code to visit Frontend Mentor and take your coding skills to the next level';
 
 function App() {
   return (
-    <BrowserRouter basename={routes.homePage}>
-      <main className="text-[#000]">
-        <Routes>
-          <Route path="/" element={<HeaderLink />} />
-          <Route
-            path={routes.qrChallenge}
-            element={<Qr src={image} title={titleText} body={bodyText} />}
-          />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <main className='text-[#000]'>
+      <Routes>
+        <Route path='/' element={<HeaderLink />} />
+        <Route
+          path={routes.qrChallenge}
+          element={<Qr src={image} title={titleText} body={bodyText} />}
+        />
+      </Routes>
+    </main>
   );
 }
 
