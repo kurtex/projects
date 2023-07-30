cc8af135d73969a08c742fc1ef1ee2f16b9bc3cb
Juan José Villarreal <jjvillarreal@MacBook-Air-de-Juan.local>
2023-07-30 16:46:12 +0200

    QR project validations fixed

--- src/components/Qr.jsx
+++ src/components/Qr.jsx
@@ -1,17 +1,15 @@
 export const Qr = ({ src, title, body }) => {
   return (
-    <main className="main">
-      <div className="border rounded-2xl p-4 bg-white w-[321px]">
-        <img className="qr-image rounded-xl" src={src} />
-        <section className="px-[1.4rem] py-6">
-          <h2 className="image-title text-[#1f3251] text-[21px]/7 font-extrabold pb-3.5">
-            {title}
-          </h2>
-          <span className="body-title text-[#7b879d] text-[15px] leading-4">
-            {body}
-          </span>
-        </section>
-      </div>
-    </main>
+    <div className='border rounded-2xl p-4 bg-white w-[321px]'>
+      <img className='qr-image rounded-xl' src={src} alt='QR image' />
+      <section className='px-[1.4rem] py-6'>
+        <h2 className='image-title text-[#1f3251] text-[21px]/7 font-extrabold pb-3.5'>
+          {title}
+        </h2>
+        <span className='body-title text-[#7b879d] text-[15px] leading-4'>
+          {body}
+        </span>
+      </section>
+    </div>
   );
 };
